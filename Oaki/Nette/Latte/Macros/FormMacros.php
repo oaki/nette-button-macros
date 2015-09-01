116a77eff153b930b6d8148a420b7fed11557abb
pavolbincik <pavolbincik@gmail.com>
2015-09-01 13:19:24 +0200

    latte 2.3.4

--- Oaki/Nette/Latte/Macros/FormMacros.php
+++ Oaki/Nette/Latte/Macros/FormMacros.php
@@ -15,59 +15,56 @@ use Nette\Latte;
 class FormMacros extends MacroSet
 {
 
-	/**
-	 * Register latte macros
-	 * @author Pavel Železný <info@pavelzelezny.cz>
-	 * @return void
-	 */
-	public static function install(Compiler $compiler)
-	{
-		parent::install($compiler);
-		$me = new static($compiler);
-		$me->addMacro('button', array($me, 'macroButton'), array($me, 'macroButtonEnd'));
-		$me->addMacro('caption', array($me, 'macroCaption'));
-	}
+    /**
+     * Register latte macros
+     * @author Pavel Železný <info@pavelzelezny.cz>
+     * @return void
+     */
+    public static function install(Compiler $compiler)
+    {
+        parent::install($compiler);
+        $me = new static($compiler);
+        $me->addMacro('button', array($me, 'macroButton'), array($me, 'macroButtonEnd'));
+        $me->addMacro('caption', array($me, 'macroCaption'));
+    }
 
 
-	public function macroButton(MacroNode $node, PhpWriter $writer)
-	{
-		$code = '$_input = (is_object(%node.word) ? %node.word : $_form[%node.word]);';
-		$code .= '$_attributes[$_input->getName()] = %node.array;';
-		$code .= '$_buttonAttrs = $_input->getControl()->attrs;';
-		$code .= '$_buttonCaption = isset($_buttonAttrs[\'value\']) === TRUE ? $_buttonAttrs[\'value\'] : NULL;';
-		$code .= 'unset($_buttonAttrs[\'type\'], $_buttonAttrs[\'value\']);';
-		$code .= '$_buttonAttrs[\'type\'] = \'submit\';'; // Prevent button type="image"
-		$code .= '$_buttonControl = \Nette\Utils\Html::el(\'button\')->addAttributes(array_merge((array) $_buttonAttrs,$_attributes[$_input->getName()]));';
-		$code .= 'echo $_buttonControl->startTag();';
-		return $writer->write($code);
-	}
+    public function macroButton(MacroNode $node, PhpWriter $writer)
+    {
+        $code = '$_input = (is_object(%node.word) ? %node.word : $_form[%node.word]);';
+        $code .= '$_attributes[$_input->getName()] = %node.array;';
+        $code .= '$_buttonAttrs = $_input->getControl()->attrs;';
+        $code .= '$_buttonCaption = isset($_buttonAttrs[\'value\']) === TRUE ? $_buttonAttrs[\'value\'] : NULL;';
+        $code .= 'unset($_buttonAttrs[\'type\'], $_buttonAttrs[\'value\']);';
+        $code .= '$_buttonAttrs[\'type\'] = \'submit\';'; // Prevent button type="image"
+        $code .= '$_buttonControl = \Nette\Utils\Html::el(\'button\')->addAttributes(array_merge((array) $_buttonAttrs,$_attributes[$_input->getName()]));';
+        $code .= 'echo $_buttonControl->startTag();';
+        return $writer->write($code);
+    }
 
 
-	public function macroButtonEnd(MacroNode $node, PhpWriter $writer)
-	{
-		$code = 'echo $_buttonControl->endTag();';
-		$code .= 'unset($_buttonControl);';
-		$code .= 'unset($_buttonCaption);';
-		$code .= 'unset($_buttonAttrs);';
-		$code .= 'unset($_attributes);';
-		$code .= 'unset($_input);';
-		return $writer->write($code);
-	}
+    public function macroButtonEnd(MacroNode $node, PhpWriter $writer)
+    {
+        $code = 'echo $_buttonControl->endTag();';
+        $code .= 'unset($_buttonControl);';
+        $code .= 'unset($_buttonCaption);';
+        $code .= 'unset($_buttonAttrs);';
+        $code .= 'unset($_attributes);';
+        $code .= 'unset($_input);';
+        return $writer->write($code);
+    }
 
 
-	public function macroCaption(MacroNode $node, PhpWriter $writer)
-	{
-		if ($node->args !== '')
-		{
-			$code = '$_input = (is_object(%node.word) ? %node.word : $_form[%node.word]);';
-			$code .= 'echo isset($_input->getControl()->attrs[\'value\']) === TRUE ? $_input->getControl()->attrs[\'value\'] : NULL;';
-			$code .= 'unset($_input);';
-		}
-		else
-		{
-			$code = 'echo isset($_buttonCaption) ? $_buttonCaption : NULL;';
-		}
-		return $writer->write($code);
-	}
+    public function macroCaption(MacroNode $node, PhpWriter $writer)
+    {
+        if ($node->args !== '') {
+            $code = '$_input = (is_object(%node.word) ? %node.word : $_form[%node.word]);';
+            $code .= 'echo isset($_input->getControl()->attrs[\'value\']) === TRUE ? $_input->getControl()->attrs[\'value\'] : NULL;';
+            $code .= 'unset($_input);';
+        } else {
+            $code = 'echo isset($_buttonCaption) ? $_buttonCaption : NULL;';
+        }
+        return $writer->write($code);
+    }
 
 }
